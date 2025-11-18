15c53bbea8403bd747495adf863ba49393fafa4e
Juan José Pérez Lazarte <n00220488@upn.pe>
2025-11-18 13:17:37 -0500

    feat(workflow) : implement workflow for git Actions

---
+++ Database/migration-initial.sql
@@ -0,0 +1,129 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Roles] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(50) NOT NULL,
+    [Description] nvarchar(300) NOT NULL,
+    [IsActive] bit NOT NULL DEFAULT CAST(1 AS bit),
+    [CreatedAt] datetime2 NOT NULL DEFAULT (GETDATE()),
+    [CreatedBy] int NULL,
+    [UpdatedAt] datetime2 NULL,
+    [UpdatedBy] int NULL,
+    [DeactivatedAt] datetime2 NULL,
+    [DeactivatedBy] int NULL,
+    CONSTRAINT [PK_Roles] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Users] (
+    [Id] int NOT NULL IDENTITY,
+    [Email] nvarchar(100) NOT NULL,
+    [PasswordHash] nvarchar(max) NOT NULL,
+    [FirstName] nvarchar(100) NOT NULL,
+    [LastName] nvarchar(100) NOT NULL,
+    [ImageUrl] nvarchar(max) NULL,
+    [DocumentType] nvarchar(20) NULL,
+    [DocumentNumber] nvarchar(20) NULL,
+    [BirthDate] datetime2 NULL,
+    [Phone] nvarchar(20) NULL,
+    [Mobile] nvarchar(20) NULL,
+    [Gender] nvarchar(20) NULL,
+    [Address] nvarchar(200) NULL,
+    [IsActive] bit NOT NULL DEFAULT CAST(1 AS bit),
+    [IsBlocked] bit NOT NULL DEFAULT CAST(0 AS bit),
+    [CreatedAt] datetime2 NOT NULL DEFAULT (GETDATE()),
+    [CreatedBy] int NULL,
+    [UpdatedAt] datetime2 NULL,
+    [UpdatedBy] int NULL,
+    [DeactivatedAt] datetime2 NULL,
+    [DeactivatedBy] int NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [RoleClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [RoleId] int NOT NULL,
+    [ClaimType] nvarchar(50) NOT NULL,
+    [ClaimValue] nvarchar(100) NOT NULL,
+    [IsEnabled] bit NOT NULL DEFAULT CAST(1 AS bit),
+    [CreatedAt] datetime2 NOT NULL DEFAULT (GETDATE()),
+    [CreatedBy] int NULL,
+    [UpdatedAt] datetime2 NULL,
+    [UpdatedBy] int NULL,
+    [DeactivatedAt] datetime2 NULL,
+    [DeactivatedBy] int NULL,
+    CONSTRAINT [PK_RoleClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_RoleClaims_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [UserClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] int NOT NULL,
+    [ClaimType] nvarchar(50) NOT NULL,
+    [ClaimValue] nvarchar(100) NOT NULL,
+    [IsEnabled] bit NOT NULL DEFAULT CAST(1 AS bit),
+    [CreatedAt] datetime2 NOT NULL DEFAULT (GETDATE()),
+    [CreatedBy] int NULL,
+    [UpdatedAt] datetime2 NULL,
+    [UpdatedBy] int NULL,
+    [DeactivatedAt] datetime2 NULL,
+    [DeactivatedBy] int NULL,
+    CONSTRAINT [PK_UserClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_UserClaims_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [UserRefreshTokens] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] int NOT NULL,
+    [JwtId] nvarchar(max) NOT NULL,
+    [RefreshTokenHash] nvarchar(max) NOT NULL,
+    [IsRevoked] bit NOT NULL DEFAULT CAST(0 AS bit),
+    [CreatedAt] datetime2 NOT NULL DEFAULT (GETDATE()),
+    [ExpirationDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_UserRefreshTokens] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_UserRefreshTokens_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [UserRoles] (
+    [UserId] int NOT NULL,
+    [RoleId] int NOT NULL,
+    [IsAssigned] bit NOT NULL DEFAULT CAST(1 AS bit),
+    [CreatedAt] datetime2 NOT NULL DEFAULT (GETDATE()),
+    [CreatedBy] int NULL,
+    [UpdatedAt] datetime2 NULL,
+    [UpdatedBy] int NULL,
+    [DeactivatedAt] datetime2 NULL,
+    [DeactivatedBy] int NULL,
+    CONSTRAINT [PK_UserRoles] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_UserRoles_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_UserRoles_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE NO ACTION
+);
+
+CREATE INDEX [IX_RoleClaims_RoleId] ON [RoleClaims] ([RoleId]);
+
+CREATE UNIQUE INDEX [IX_Roles_Name] ON [Roles] ([Name]);
+
+CREATE INDEX [IX_UserClaims_UserId] ON [UserClaims] ([UserId]);
+
+CREATE INDEX [IX_UserRefreshTokens_UserId] ON [UserRefreshTokens] ([UserId]);
+
+CREATE INDEX [IX_UserRoles_RoleId] ON [UserRoles] ([RoleId]);
+
+CREATE UNIQUE INDEX [IX_Users_DocumentNumber] ON [Users] ([DocumentNumber]) WHERE [DocumentNumber] IS NOT NULL;
+
+CREATE UNIQUE INDEX [IX_Users_Email] ON [Users] ([Email]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251116193131_CreateInitialScheme', N'9.0.10');
+
+COMMIT;
+GO
+
