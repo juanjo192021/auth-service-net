5c3b4ffdcb17b375a2c00e44a442bc2407afdba2
Juan José Pérez Lazarte <n00220488@upn.pe>
2025-11-22 11:38:53 -0500

    feat(deactivate-role): add DeactivateRoleHandler using MediatR

---
+++ Authentication.RefreshToken.Application.UseCases/Role/Commands/DeactivateRole/DeactivateRoleCommand.cs
@@ -0,0 +1,10 @@
+﻿using Authentication.RefreshToken.Concerns.Common;
+using MediatR;
+
+namespace Authentication.RefreshToken.Application.UseCases.Role.Commands.DeactivateRole
+{
+    public sealed record class DeactivateRoleCommand : IRequest<SuccessResponse<bool>>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Authentication.RefreshToken.Application.UseCases/Role/Commands/DeactivateRole/DeactivateRoleHandler.cs
@@ -0,0 +1,29 @@
+﻿using Authentication.RefreshToken.Application.Interfaces.Persistence;
+using Authentication.RefreshToken.Application.UseCases.Common.Exceptions;
+using Authentication.RefreshToken.Concerns.Common;
+using MapsterMapper;
+using MediatR;
+
+namespace Authentication.RefreshToken.Application.UseCases.Role.Commands.DeactivateRole
+{
+    public class DeactivateRoleHandler : IRequestHandler<DeactivateRoleCommand, SuccessResponse<bool>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DeactivateRoleHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<SuccessResponse<bool>> Handle(DeactivateRoleCommand request, CancellationToken cancellationToken)
+        {
+            var response = new SuccessResponse<bool>();
+            var isDeactivated = await _unitOfWork.Roles.DeactivateAsync(request.Id);
+            if (!isDeactivated)
+                throw new NotFoundException($"Not found role with ID {request.Id}");
+            
+            //response.Data = isDeactivated;
+            response.Message = "Role deactivated successfully.";
+            return response;
+        }
+    }
+}
