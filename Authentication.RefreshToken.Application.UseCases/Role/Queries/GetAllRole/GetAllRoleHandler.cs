c394f8ec216d90670aca73fc83e44fe9a46960ab
Juan José Pérez Lazarte <n00220488@upn.pe>
2025-11-22 11:47:31 -0500

    feat(getall-role): add GetAllRoleQuery and GetAllRoleHandler with MediatR

---
+++ Authentication.RefreshToken.Application.UseCases/Role/Queries/GetAllRole/GetAllRoleHandler.cs
@@ -0,0 +1,44 @@
+﻿using Authentication.RefreshToken.Application.Dto.Role;
+using Authentication.RefreshToken.Application.Interfaces.Persistence;
+using Authentication.RefreshToken.Application.UseCases.Common.Exceptions;
+using Authentication.RefreshToken.Concerns.Common;
+using MapsterMapper;
+using MediatR;
+
+namespace Authentication.RefreshToken.Application.UseCases.Role.Queries.GetAllRole
+{
+    public class GetAllRoleHandler : IRequestHandler<GetAllRoleQuery, ResponsePagination<IEnumerable<RoleDto>>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetAllRoleHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponsePagination<IEnumerable<RoleDto>>> Handle(GetAllRoleQuery request, CancellationToken cancellationToken)
+        {
+            var response = new ResponsePagination<IEnumerable<RoleDto>>();
+
+            var pageNumber = request.PageNumber;
+            var pageSize = request.PageSize;
+            var search = request.Search;
+
+            var count = await _unitOfWork.Roles.CountAsync();
+            if (count < 1)
+                throw new NotFoundException("Not found any roles");
+
+            var roles = await _unitOfWork.Roles.GetAllAsync(pageNumber, pageSize, search);
+
+            response.Data = _mapper.Map<IEnumerable<RoleDto>>(roles);
+            response.Message = "Roles retrieved successfully";
+            response.PageNumber = pageNumber;
+            response.TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            response.TotalCount = count;
+
+            return response;
+        }
+    }
+}
---
+++ Authentication.RefreshToken.Application.UseCases/Role/Queries/GetAllRole/GetAllRoleQuery.cs
@@ -0,0 +1,13 @@
+﻿using Authentication.RefreshToken.Application.Dto.Role;
+using Authentication.RefreshToken.Concerns.Common;
+using MediatR;
+
+namespace Authentication.RefreshToken.Application.UseCases.Role.Queries.GetAllRole
+{
+    public sealed record class GetAllRoleQuery : IRequest<ResponsePagination<IEnumerable<RoleDto>>>
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? Search { get; set; } = string.Empty;
+    }
+}
