e30de512391ad0e6f1761660f6966d5b0bdeb042
Juan José Pérez Lazarte <n00220488@upn.pe>
2025-11-22 12:04:27 -0500

    feat(role-controller): add new role endpoints

--- Authentication.RefreshToken.Services.WebApi/Controllers/v1/RolesController.cs
+++ Authentication.RefreshToken.Services.WebApi/Controllers/v1/RolesController.cs
@@ -1,6 +1,10 @@
 ﻿using Asp.Versioning;
 using Authentication.RefreshToken.Application.Dto.Role;
-using Authentication.RefreshToken.Application.UseCases.Role.Queries.GetAllRoleQuery;
+using Authentication.RefreshToken.Application.UseCases.Role.Commands.CreateRole;
+using Authentication.RefreshToken.Application.UseCases.Role.Commands.DeactivateRole;
+using Authentication.RefreshToken.Application.UseCases.Role.Commands.UpdateRole;
+using Authentication.RefreshToken.Application.UseCases.Role.Queries.GetAllRole;
+using Authentication.RefreshToken.Application.UseCases.Role.Queries.GetRole;
 using Authentication.RefreshToken.Concerns.Common;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -23,6 +27,39 @@ namespace Authentication.RefreshToken.Services.WebApi.Controllers.v1
             _mediator = mediator;
         }
 
+        [HttpPost]
+        [SwaggerOperation(
+            Summary = "Create Role",
+            Description = "Create a new role in the system",
+            OperationId = "CreateRole"
+        )]
+        [SwaggerResponse(StatusCodes.Status200OK, "OK", typeof(SuccessResponse<RoleDto>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "BadRequest", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Not Found", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status409Conflict, "Conflict", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal Server error", typeof(ErrorResponse))]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateRoleCommand command)
+        {
+            var response =  await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id:int}")]
+        [SwaggerOperation(
+            Summary = "Deactivate Role",
+            Description = "Deactivate a role by its identifier",
+            OperationId = "DeactivateRole"
+        )]
+        [SwaggerResponse(StatusCodes.Status200OK, "OK", typeof(SuccessResponse<bool>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "BadRequest", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Not Found", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal Server error", typeof(ErrorResponse))]
+        public async Task<IActionResult> DeactivateAsync(int id)
+        {
+            var response = await _mediator.Send(new DeactivateRoleCommand() { Id = id});
+            return Ok(response);
+        }
+
         [HttpGet]
         [SwaggerOperation(
             Summary = "List of Roles",
@@ -36,6 +73,39 @@ namespace Authentication.RefreshToken.Services.WebApi.Controllers.v1
         public async Task<IActionResult> GetAllAsync([FromQuery] GetAllRoleQuery query)
         {
             var response = await _mediator.Send(query);
+            return Ok(response);
+        }
+
+        [HttpGet("{id:int}")]
+        [SwaggerOperation(
+            Summary = "Get Role by Id",
+            Description = "Obtain a role by its identifier",
+            OperationId = "GetRoleById"
+        )]
+        [SwaggerResponse(StatusCodes.Status200OK, "OK", typeof(SuccessResponse<RoleDto>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "BadRequest", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Not Found", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal Server error", typeof(ErrorResponse))]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var response = await _mediator.Send(new GetRoleQuery() { Id = id});
+            return Ok(response);
+        }
+
+        [HttpPatch]
+        [SwaggerOperation(
+            Summary = "Update Role",
+            Description = "Update an existing role in the system",
+            OperationId = "UpdateRole"
+        )]
+        [SwaggerResponse(StatusCodes.Status200OK, "OK", typeof(SuccessResponse<RoleDto>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "BadRequest", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Not Found", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status409Conflict, "Conflict", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal Server error", typeof(ErrorResponse))]
+        public async Task<IActionResult> UpdateAsync([FromRoute] UpdateRoleCommand command)
+        {
+            var response = await _mediator.Send(command);
             return Ok(response);
         }
     }
